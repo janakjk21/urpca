4371ee36b15ecc9b19dcb4085e687038a82ad1ec
janaksapkota5 <92215890+janaksapkota5@users.noreply.github.com>
2023-09-13 13:49:51 +0545

    updated workflow

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+	return <div>janak</div>;
+};
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,8 @@ import '@testing-library/jest-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import rootReducer from './components/redux';
-import { router } from './router';
+
+import App from './App';
 
 import 'regenerator-runtime/runtime.js';
 
@@ -14,7 +15,7 @@ const store = configureStore({ reducer: rootReducer });
 test('renders learn react link', () => {
 	const { getByText } = render(
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<App />
 		</Provider>
 	);
 	const linkElement = getByText(/learn react/i);
